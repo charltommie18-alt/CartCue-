f81291f5f47dc9f4a70dcadb995a10fe07897e8f
charltommie18-alt <charltommie18@gmail.com>
2026-08-28 18:51:43 +0200

    Update MainActivity.java

--- amazon-iap/MainActivity.java
+++ amazon-iap/MainActivity.java
@@ -8,59 +8,56 @@ import com.getcapacitor.BridgeActivity;
 import java.util.HashSet;
 import java.util.Set;
 
-public class MainActivity extends BridgeActivity {
-
-    private static final String PARENT_SKU =
-            "CartCue_monthly_sub";
-
-    private static final String SUBSCRIPTION_SKU =
-            "CartCue_monthly_term";
+public class MainActivity
+        extends BridgeActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(AmazonIAPPlugin.class);
+    public void onCreate(
+            Bundle savedInstanceState
+    ) {
 
-        super.onCreate(savedInstanceState);
+        registerPlugin(
+                AmazonIAPPlugin.class
+        );
+
+        super.onCreate(
+                savedInstanceState
+        );
     }
 
     @Override
     public void onResume() {
+
         super.onResume();
 
-        /*
-         * Amazon Appstore IAP calls are safe to make
-         * when the app is running inside Amazon.
-         *
-         * Outside Amazon Appstore, these calls may
-         * fail, so they are intentionally protected.
-         */
         try {
+
             PurchasingService.getUserData();
 
             Set<String> productSkus =
                     new HashSet<>();
 
-            productSkus.add(PARENT_SKU);
-            productSkus.add(SUBSCRIPTION_SKU);
+            productSkus.add(
+                    "CartCue_monthly_sub"
+            );
+
+            productSkus.add(
+                    "CartCue_monthly_term"
+            );
 
             PurchasingService.getProductData(
                     productSkus
             );
 
-            /*
-             * false means:
-             * start from the last synchronized
-             * purchase-update position.
-             */
             PurchasingService.getPurchaseUpdates(
                     false
             );
 
         } catch (Exception ignored) {
             /*
-             * CartCue can still run normally outside
-             * the Amazon Appstore environment.
+             * Amazon Appstore may not be available
+             * outside the Amazon Appstore environment.
              */
         }
     }
-}
+        }
